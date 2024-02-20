8bd05384ea8433f80f2cfe4ec29b77118cc903f0
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-02-20 12:24:19 +0000

    shellcheck
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-IMAGE="${1}"
 IMAGE_TAG="analytical-platform.service.justice.gov.uk/visual-studio-code:local"
 CONTAINER_STRUCTURE_TEST_IMAGE="gcr.io/gcp-runtimes/container-structure-test:latest"
 
@@ -9,11 +8,12 @@ if [[ "${REMOTE_CONTAINERS}" ]] && [[ "$(uname -m)" == "aarch64" ]]; then
   echo "(⚠) This script builds linux/amd64 images which might take a long time or even fail."
   export PLATFORM_FLAG="--platform linux/amd64"
 fi
-
+# shellcheck disable=SC2086 special case for PLATFORM_FLAG
 docker build ${PLATFORM_FLAG} --file Dockerfile --tag "${IMAGE_TAG}" .
 
 echo "Running container structure test for [ ${IMAGE_TAG} ]"
 
+# shellcheck disable=SC2086 special case for PLATFORM_FLAG
 docker run --rm ${PLATFORM_FLAG} \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume "${PWD}:/workspace" \
